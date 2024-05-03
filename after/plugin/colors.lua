1b0d3bed75d2a992a938f301a31f211fa89e3d8a
Bozhidar Tsvetkov <bozhidartsvetkov21@gmail.com>
2024-05-03 06:45:25 +0300

    Color relative line numbers

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,6 +3,9 @@ function ColorMyPencils(color)
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#C3FAF0', bold=false })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=false })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#E1A9F5', bold=false })
 end
 
 ColorMyPencils()
