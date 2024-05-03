b9cc9b79387cb0c9a92fdf6e05aa83ab3937201f
Bozhidar Tsvetkov <bozhidartsvetkov21@gmail.com>
2024-05-03 21:08:43 +0300

    Change comments color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,6 +6,7 @@ function ColorMyPencils(color)
     vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#C3FAF0', bold=false })
     vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=false })
     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#E1A9F5', bold=false })
+    vim.api.nvim_set_hl(0, 'Comment', { fg='#8EA302'})
 end
 
 ColorMyPencils()
