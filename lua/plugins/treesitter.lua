974b78ec0612ddc897f88e6ea1cf6b23277516df
Bozhidar Tsvetkov <bozhidar.tsvetkov@ocado.com>
2024-05-28 13:33:35 +0300

    Fix treesitter for Mac

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,5 +4,23 @@ return {
     build = ":TSUpdate",
     branch = "main",
     lazy = false,
+    config = function ()
+        require'nvim-treesitter.config'.setup {
+          -- A list of parser names, or "all" (the five listed parsers should always be installed)
+          ensure_installed = {
+            "c",
+            "lua",
+            "rust",
+            "kotlin",
+            "json",
+            "java",
+            "go",
+            "typescript",
+            "javascript",
+            "scala",
+            "vim"
+          },
+      }
+    end
   },
 }
